3fe5902acfcdcce7ef81015c5321201b60ee1210
Alex <basurillodesgastorito@gmail.com>
2023-12-18 12:59:33 +0100

    Initial upload

---
+++ main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "simula.h"
+
+struct coor{
+  int x, y;
+};
+
+
+void on_start(){
+  struct coor base;
+  sensor_t rob;
+  float angle;
+  rmb_awake(&base.x, &base.y);
+  //random heading in a 180º arc
+  rob = rmb_state();
+  angle = rand()  / (float)RAND_MAX * M_PI - M_PI_2; 
+  rmb_turn(rob.head + angle);
+}
+
+
+void cyclic_behav(){
+  //gira un ángulo aleatorio
+  float angle = rand() / (float)RAND_MAX * 2 * M_PI;
+  rmb_turn(angle);
+  //da un paso en la dirección actual
+  rmb_forward();
+  //detección de obstáculos
+  if(rmb_bumper()){
+    //gira un ángulo aleatorio
+    angle = rand() / (float)RAND_MAX * 2 * M_PI;
+    rmb_turn(angle);
+  }
+  //deteccion de suciedad
+  while(rmb_ifr() > 0)
+    rmb_clean();
+  //carga si se encuentra en la base
+  if(rmb_at_base())
+    while(rmb_battery() < 250)
+      rmb_load();
+}
+
+
+void on_stop(){
+  visualize();
+}
+
+
+int main(){
+  configure(on_start, cyclic_behav, on_stop, 500);
+  run();
+  return 0;
+}
